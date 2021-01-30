04607ad0fc148f96d37c36113eb14f42e5a80556
Anton <say.unmomento@gmail.com>
2021-01-30 22:33:08 +0600

    power

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo continueKey;
+            double number, power, result;
+            Console.WriteLine("Программа для возведения числа в степень");
+            while (true)
+            {
+                Console.Write("Введите число: ");
+                double.TryParse(Console.ReadLine(), out number); ;
+                Console.Write("Введите степень: ");
+                double.TryParse(Console.ReadLine(), out power); ;
+                result = Math.Pow(number, power);
+                Console.WriteLine("Результат: " + result);
+                Console.WriteLine("Для продолжения нажмите любую кнопку, для остановки программы нажмите N");
+                Console.Write("Ожидание действий: ");
+                continueKey = Console.ReadKey();
+                Console.WriteLine();
+                if (continueKey.Key == ConsoleKey.N)
+                    Environment.Exit(0);
+            }
+        }
+    }
+}
